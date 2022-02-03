4e657b8e49d9308ed4516f6bfd6285ca656ebf1a
tatsuya.nakanishi <jwtja6wv@gmail.com>
2022-02-03 01:39:02 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = '17053eac4501425fab09e4f15313eeb9.vfs.cloud9.us-east-2.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
